29761b59bb5bd87a486245cf1e69de3da0c31c6f
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2023-10-18 10:21:53 +0200

    String NullSafeBox as a separate class

--- src/main/java/fr/sncf/d2d/up2dev/StringNullSafeBox.java
+++ src/main/java/fr/sncf/d2d/up2dev/StringNullSafeBox.java
@@ -1,22 +1,14 @@
 package fr.sncf.d2d.up2dev;
 
-public class StringNullSafeBox extends NullSafeBox {
+public class StringNullSafeBox extends NullSafeBox<String> {
 
     public StringNullSafeBox(String value) {
         super(value);
     }
 
     @Override
-    public String getValue() {
-        return (String) super.getValue();
-    }
-
-    @Override
-    public void setValue(Object value) {
-        if (value instanceof String) {
-            super.setValue(value);
-        } else {
-            throw new IllegalStateException();
-        }
+    public String getOrDefault(String alt) {
+        String str = super.getOrDefault(alt);
+        return str == null ? "" : str;
     }
 }
