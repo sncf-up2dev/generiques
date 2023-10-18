8e8bc4add64132e80bf92f01af1234b6b94f2376
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2023-10-18 11:51:19 +0200

    Utilisation de joker

--- src/main/java/fr/sncf/d2d/up2dev/Main.java
+++ src/main/java/fr/sncf/d2d/up2dev/Main.java
@@ -20,5 +20,33 @@ public class Main {
 
         String str3 = nulBox.getOrThrow(new IllegalStateException());
         System.out.println(str3);
+
+        boolean b;
+        b = isEmpty(intBox);
+        b = isEmpty(strBox);
+        b = isEmpty(objBox);
+
+        NullSafeBox<?> box;
+
+        box = intBox;
+        box = strBox;
+        box = objBox;
+
+        b = box.getValue() == null;
+
+        Object o = box.getValue();
+
+        NullSafeBox<Double> dblBox = new NullSafeBox<>(3.1);
+
+        b = isEmptyOrZero(intBox);
+        b = isEmptyOrZero(dblBox);
+    }
+
+    static boolean isEmpty(NullSafeBox<?> box) {
+        return box.getValue() == null;
+    }
+
+    static boolean isEmptyOrZero(NullSafeBox<? extends Number> box) {
+        return box.getValue() == null && box.getValue().intValue() == 0;
     }
 }
